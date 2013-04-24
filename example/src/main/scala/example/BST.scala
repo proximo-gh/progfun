874327c4f68230563182fe1cc2777c3ff5cbda58
proximo <vnlobanovs@gmail.com>
2013-04-25 00:16:12 +0400

    BST

--- example/src/main/scala/example/BST.scala
+++ example/src/main/scala/example/BST.scala
@@ -4,30 +4,22 @@ object BST {
   def apply() = Empty
 }
 
-abstract class BST {
-  abstract def contains(elem: Int): Boolean
+trait BST {
 
-  abstract def add(elem: Int): BST
+  def contains(elem: Int): Boolean
+
+  def add(elem: Int): BST
 }
 
 object Empty extends BST {
+
   def contains(elem: Int): Boolean = false
 
-  def add(elem: Int): BST = new Leaf(elem)
+  def add(elem: Int): BST = new Node(elem, Empty, Empty)
 }
 
-case class Leaf(elem: Int) extends BST{
-  def contains(elem: Int): Boolean = {
-    this.elem == elem
-  }
+case class Node(elem: Int, left: BST, right: BST) extends BST {
 
-  def add(elem: Int): BST = {
-    if (elem < this.elem) new Node(this.elem, new Leaf(elem), Empty)
-    else new Node(this.elem, Empty, new Leaf(elem))
-  }
-}
-
-case class Node(elem: Int, left: BST, right: BST) extends BST{
   def contains(elem: Int): Boolean = {
     if (this.elem == elem) true
     else if (elem < this.elem) left.contains(elem)
@@ -38,6 +30,5 @@ case class Node(elem: Int, left: BST, right: BST) extends BST{
     if (this.elem == elem) this
     else if (elem < this.elem) new Node(this.elem, left.add(elem), right)
     else new Node(this.elem, left, right.add(elem))
-
   }
 }
