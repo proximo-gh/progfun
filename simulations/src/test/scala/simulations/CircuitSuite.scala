835a184ab9beff5fc9061a670b5c565a0dd90eac
proximo <vnlobanovs@gmail.com>
2013-11-14 23:59:31 +0400

    simulation or gates tests

--- simulations/src/test/scala/simulations/CircuitSuite.scala
+++ simulations/src/test/scala/simulations/CircuitSuite.scala
@@ -14,18 +14,18 @@ class CircuitSuite extends CircuitSimulator with FunSuite {
   test("andGate example") {
     val in1, in2, out = new Wire
     andGate(in1, in2, out)
-    in1.setSignal(false)
-    in2.setSignal(false)
+    in1.setSignal(off)
+    in2.setSignal(off)
     run
 
     assert(out.getSignal === false, "and 1")
 
-    in1.setSignal(true)
+    in1.setSignal(on)
     run
 
     assert(out.getSignal === false, "and 2")
 
-    in2.setSignal(true)
+    in2.setSignal(on)
     run
 
     assert(out.getSignal === true, "and 3")
